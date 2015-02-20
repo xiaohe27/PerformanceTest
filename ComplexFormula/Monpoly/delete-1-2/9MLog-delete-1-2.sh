559a0ea490576e5242fd4c1e439fbfc11d6c687a
xiaohe27 <xiaoguoyi27@gmail.com>
2015-02-20 10:19:34 -0600

    ok

---
+++ ComplexFormula/Monpoly/delete-1-2/9MLog-delete-1-2.sh
@@ -0,0 +1,10 @@
+Out=timeFor9MLog-Mon-Siebel.txt
+Start=$(date +"%s")
+echo "Start time : $Start" > $Out
+
+/home/xiaohe/SW/offline-log-analysis/existingApp/monpoly-1.1.2/monpoly -sig delete.sig -formula delete12.mfotl -negate -log /home/xiaohe/workspace/DATA/MeasureBaseTime/ldcc4Monpoly_buggy > violations.txt
+
+End=$(date +"%s")
+echo "Finish time : $End" >> $Out
+Diff=$(( $End - $Start ))
+echo "\nIt took Monpoly (opt) $Diff seconds to output all violations of delete-1-2 Property in the 300MB log file!" >> $Out
